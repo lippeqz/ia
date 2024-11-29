f75c426221e57ca2a5e191d50a1232f5e6c9afd5
andre.willer <126670653+lippeqz@users.noreply.github.com>
2024-11-29 08:53:26 -0300

    Update informacoes.js

--- informacoes.js
+++ informacoes.js
@@ -1,18 +1,18 @@
-const url = 'https://raw.githubusercontent.com/Akiovictor/trabalho_json_grafico/refs/heads/main/informacoes.json'
+const url = 'https://raw.githubusercontent.com/XxAndressaxX/graficos/refs/heads/main/informacoes.json'
 
 async function vizualizarInformacoes() {
     const res = await fetch(url)
     const dados = await res.json()
-    const quantidade_alunos = (dados.numero_alunos)
-    const numero_votos = (dados.total_votos)
-    const numero_aula_Victor = (dados.total_aula_profVictor)
-    const numero_aula_Mazza = (dados.total_aula_semana)
-    const porcentagem_Victor = ((numero_aula_Victor / numero_aula_Mazza ) * 100)
+ 
+    const quantidade_estudantes = (dados.total_estudantes)
+    const quantidade_alunos_interessados= (dados.quantidade_querem_faculdade)
+    const porcentagem_querem_faculdade = ((quantidade_alunos_interessados / quantidade_estudantes)* 100).toFixed(0)
+    const alunos_querem_saude = (dados.total_alunos_saude)
+
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `melhores jogos<span> de <span> 2024 <span>
-
+    paragrafo.innerHTML = `Você sabia que em uma pesquisa <span>realizada pelo IBGE</span> feita com <span>${quantidade_estudantes} jovens </span> apenas <span>${porcentagem_querem_faculdade}% </span> querem ingressar na faculdade? <br> Isso representa <span> ${quantidade_alunos_interessados} adolescentes </span>. Além disso, dentre os cursos disponíveis, os da <span>área da saúde</span> são desejados por <span>${alunos_querem_saude} jovens. </span>`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
